1b5cb246b550a50a866b4e1f47d0b6a727d9affd
Carbon2301 <trinhhuuan372@gmail.com>
2025-06-08 23:30:34 +0700

    Sua duong dan seedFacilities.js

--- backend/scripts/seedFacilities.js
+++ backend/scripts/seedFacilities.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Facility = require('./models/facilityModel');
+const Facility = require('../models/facilityModel');
 
 dotenv.config();
 
